b060a5040e255694f3283a28bee9e4f7cf54dec1
Jared Jensen <jared@cloudsy.com>
2017-01-24 15:03:45 -0500

    Add hue/saturation support for MQTT lights

--- homeassistant/components/light/mqtt.py
+++ homeassistant/components/light/mqtt.py
@@ -5,6 +5,7 @@ For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/light.mqtt/
 """
 import logging
+import colorsys
 
 import voluptuous as vol
 
@@ -19,6 +20,9 @@ from homeassistant.const import (
 from homeassistant.components.mqtt import (
     CONF_STATE_TOPIC, CONF_COMMAND_TOPIC, CONF_QOS, CONF_RETAIN)
 import homeassistant.helpers.config_validation as cv
+from homeassistant.util import color as color_util
+from homeassistant.util.color import \
+    color_temperature_mired_to_kelvin as mired_to_kelvin
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,16 +35,28 @@ CONF_BRIGHTNESS_VALUE_TEMPLATE = 'brightness_value_template'
 CONF_RGB_STATE_TOPIC = 'rgb_state_topic'
 CONF_RGB_COMMAND_TOPIC = 'rgb_command_topic'
 CONF_RGB_VALUE_TEMPLATE = 'rgb_value_template'
+CONF_HUE_STATE_TOPIC = 'hue_state_topic'
+CONF_HUE_COMMAND_TOPIC = 'hue_command_topic'
+CONF_SAT_STATE_TOPIC = 'saturation_state_topic'
+CONF_SAT_COMMAND_TOPIC = 'saturation_command_topic'
 CONF_BRIGHTNESS_SCALE = 'brightness_scale'
 CONF_COLOR_TEMP_STATE_TOPIC = 'color_temp_state_topic'
 CONF_COLOR_TEMP_COMMAND_TOPIC = 'color_temp_command_topic'
 CONF_COLOR_TEMP_VALUE_TEMPLATE = 'color_temp_value_template'
+
+CONF_COLOR_TEMP_UNIT = 'color_temp_unit'
+
+COLOR_TEMP_UNIT_KELVIN = 'kelvin'
+COLOR_TEMP_UNIT_MIRED = 'mired'
+
+BYTE_MAX = 255
 
 DEFAULT_NAME = 'MQTT Light'
 DEFAULT_PAYLOAD_ON = 'ON'
 DEFAULT_PAYLOAD_OFF = 'OFF'
 DEFAULT_OPTIMISTIC = False
 DEFAULT_BRIGHTNESS_SCALE = 255
+DEFAULT_COLOR_TEMP_UNIT = COLOR_TEMP_UNIT_MIRED
 
 PLATFORM_SCHEMA = mqtt.MQTT_RW_PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
@@ -54,13 +70,28 @@ PLATFORM_SCHEMA = mqtt.MQTT_RW_PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_RGB_STATE_TOPIC): mqtt.valid_subscribe_topic,
     vol.Optional(CONF_RGB_COMMAND_TOPIC): mqtt.valid_publish_topic,
     vol.Optional(CONF_RGB_VALUE_TEMPLATE): cv.template,
+    vol.Optional(CONF_HUE_STATE_TOPIC): mqtt.valid_subscribe_topic,
+    vol.Optional(CONF_HUE_COMMAND_TOPIC): mqtt.valid_publish_topic,
+    vol.Optional(CONF_SAT_STATE_TOPIC): mqtt.valid_subscribe_topic,
+    vol.Optional(CONF_SAT_COMMAND_TOPIC): mqtt.valid_publish_topic,
     vol.Optional(CONF_PAYLOAD_ON, default=DEFAULT_PAYLOAD_ON): cv.string,
     vol.Optional(CONF_PAYLOAD_OFF, default=DEFAULT_PAYLOAD_OFF): cv.string,
     vol.Optional(CONF_OPTIMISTIC, default=DEFAULT_OPTIMISTIC): cv.boolean,
     vol.Optional(CONF_BRIGHTNESS_SCALE, default=DEFAULT_BRIGHTNESS_SCALE):
         vol.All(vol.Coerce(int), vol.Range(min=1)),
+    vol.Optional(CONF_COLOR_TEMP_UNIT, default=DEFAULT_COLOR_TEMP_UNIT):
+        vol.Any(COLOR_TEMP_UNIT_KELVIN, COLOR_TEMP_UNIT_MIRED)
 })
 
+def convert_rgb_to_hsv(rgb):
+    """Convert Home Assistant RGB values to HSV values."""
+    red, green, blue = [_ / BYTE_MAX for _ in rgb]
+    return tuple(int(i * 100) for i in colorsys.rgb_to_hsv(red, green, blue))
+
+def convert_hsv_to_rgb(hsv):
+    """Convert normalized HSV to Home Assistant RGB values."""
+    hue, sat, val = [_ / 100 for _ in hsv]
+    return tuple(int(i * BYTE_MAX) for i in colorsys.hsv_to_rgb(hue, sat, val))
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Add MQTT Light."""
@@ -77,6 +108,10 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
                 CONF_BRIGHTNESS_COMMAND_TOPIC,
                 CONF_RGB_STATE_TOPIC,
                 CONF_RGB_COMMAND_TOPIC,
+                CONF_HUE_STATE_TOPIC,
+                CONF_HUE_COMMAND_TOPIC,
+                CONF_SAT_STATE_TOPIC,
+                CONF_SAT_COMMAND_TOPIC,
                 CONF_COLOR_TEMP_STATE_TOPIC,
                 CONF_COLOR_TEMP_COMMAND_TOPIC
             )
@@ -95,6 +130,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
         },
         config.get(CONF_OPTIMISTIC),
         config.get(CONF_BRIGHTNESS_SCALE),
+        config.get(CONF_COLOR_TEMP_UNIT),
     )])
 
 
@@ -102,7 +138,7 @@ class MqttLight(Light):
     """MQTT light."""
 
     def __init__(self, hass, name, topic, templates, qos, retain, payload,
-                 optimistic, brightness_scale):
+                 optimistic, brightness_scale, color_temp_unit):
         """Initialize MQTT light."""
         self._hass = hass
         self._name = name
@@ -117,11 +153,19 @@ class MqttLight(Light):
             optimistic or topic[CONF_BRIGHTNESS_STATE_TOPIC] is None)
         self._optimistic_color_temp = (
             optimistic or topic[CONF_COLOR_TEMP_STATE_TOPIC] is None)
+        self._optimistic_hue = (
+            optimistic or topic[CONF_HUE_STATE_TOPIC] is None)
+        self._optimistic_sat = (
+            optimistic or topic[CONF_SAT_STATE_TOPIC] is None)
         self._brightness_scale = brightness_scale
+        self._ct_unit = color_temp_unit
         self._state = False
         self._supported_features = 0
         self._supported_features |= (
-            topic[CONF_RGB_STATE_TOPIC] is not None and SUPPORT_RGB_COLOR)
+            (topic[CONF_RGB_STATE_TOPIC] is not None or
+             (topic[CONF_HUE_STATE_TOPIC] is not None and
+              topic[CONF_SAT_STATE_TOPIC] is not None)) and
+            SUPPORT_RGB_COLOR)
         self._supported_features |= (
             topic[CONF_BRIGHTNESS_STATE_TOPIC] is not None and
             SUPPORT_BRIGHTNESS)
@@ -135,6 +179,8 @@ class MqttLight(Light):
             else:
                 tpl.hass = hass
                 templates[key] = tpl.render_with_possible_json_value
+
+        self.templates = templates
 
         def state_received(topic, payload, qos):
             """A new MQTT message has been received."""
@@ -182,9 +228,45 @@ class MqttLight(Light):
         else:
             self._rgb = None
 
+        def hue_received(topic, payload, qos):
+            """A new MQTT message for hue has been received."""
+            self._hue = int(payload)
+            rgb_color = convert_hsv_to_rgb((int(payload), int(self._sat), 100))
+            self._rgb = rgb_color
+            self.update_ha_state()
+
+        if self._topic[CONF_HUE_STATE_TOPIC] is not None:
+            mqtt.subscribe(self._hass, self._topic[CONF_HUE_STATE_TOPIC],
+                           hue_received, self._qos)
+            self._hue = 0
+        if self._topic[CONF_HUE_COMMAND_TOPIC] is not None:
+            self._hue = 0
+        else:
+            self._hue = None
+
+        def sat_received(topic, payload, qos):
+            """A new MQTT message for saturation has been received."""
+            self._sat = int(payload)
+            self._rgb = convert_hsv_to_rgb((int(self._hue), int(payload), 100))
+            self.update_ha_state()
+
+        if self._topic[CONF_SAT_STATE_TOPIC] is not None:
+            mqtt.subscribe(self._hass, self._topic[CONF_SAT_STATE_TOPIC],
+                           sat_received, self._qos)
+            self._sat = 0
+        if self._topic[CONF_SAT_COMMAND_TOPIC] is not None:
+            self._sat = 0
+        else:
+            self._sat = None
+
         def color_temp_received(topic, payload, qos):
             """A new MQTT message for color temp has been received."""
-            self._color_temp = int(templates[CONF_COLOR_TEMP](payload))
+            if self._ct_unit == 'kelvin':
+                self._color_temp = int(templates[CONF_COLOR_TEMP](
+                    color_util.color_temperature_kelvin_to_mired(float(payload))))
+            else:
+                self._color_temp = int(templates[CONF_COLOR_TEMP](payload))
+
             self.update_ha_state()
 
         if self._topic[CONF_COLOR_TEMP_STATE_TOPIC] is not None:
@@ -241,12 +323,29 @@ class MqttLight(Light):
         """Turn the device on."""
         should_update = False
 
-        if ATTR_RGB_COLOR in kwargs and \
-           self._topic[CONF_RGB_COMMAND_TOPIC] is not None:
+        if ATTR_RGB_COLOR in kwargs:
+            if self._topic[CONF_RGB_COMMAND_TOPIC] is not None:
 
-            mqtt.publish(self._hass, self._topic[CONF_RGB_COMMAND_TOPIC],
-                         '{},{},{}'.format(*kwargs[ATTR_RGB_COLOR]),
-                         self._qos, self._retain)
+                mqtt.publish(self._hass, self._topic[CONF_RGB_COMMAND_TOPIC],
+                             '{},{},{}'.format(*kwargs[ATTR_RGB_COLOR]),
+                             self._qos, self._retain)
+
+            elif self._topic[CONF_HUE_COMMAND_TOPIC] is not None \
+             and self._topic[CONF_SAT_COMMAND_TOPIC] is not None:
+                hue, saturation, _ = \
+                    convert_rgb_to_hsv(kwargs[ATTR_RGB_COLOR])
+
+                mqtt.publish(
+                    self._hass, self._topic[CONF_HUE_COMMAND_TOPIC],
+                    hue, self._qos, self._retain)
+
+                mqtt.publish(
+                    self._hass, self._topic[CONF_SAT_COMMAND_TOPIC],
+                    saturation, self._qos, self._retain)
+
+                if self._optimistic_hue or self._optimistic_sat:
+                    self._hue = hue
+                    self._sat = saturation
 
             if self._optimistic_rgb:
                 self._rgb = kwargs[ATTR_RGB_COLOR]
@@ -266,7 +365,11 @@ class MqttLight(Light):
 
         if ATTR_COLOR_TEMP in kwargs and \
            self._topic[CONF_COLOR_TEMP_COMMAND_TOPIC] is not None:
-            color_temp = int(kwargs[ATTR_COLOR_TEMP])
+            if self._ct_unit == 'kelvin':
+                color_temp = int(mired_to_kelvin(kwargs[ATTR_COLOR_TEMP]))
+            else:
+                color_temp = int(kwargs[ATTR_COLOR_TEMP])
+
             mqtt.publish(
                 self._hass, self._topic[CONF_COLOR_TEMP_COMMAND_TOPIC],
                 color_temp, self._qos, self._retain)
